1123458c0b2f5046f94314e2845af22cd77da40e
George <georgemitr96@hotmail.com>
2019-08-20 02:45:55 +0300

    Added ajax POST on Login. Saved user info on local storage. Enriched user routes. Added basic buttons on welcome page.

--- src/Login.js
+++ src/Login.js
@@ -3,64 +3,103 @@ import { withRouter } from 'react-router-dom';
 import { Row, Col, Card, CardBody, CardHeader, Form, Button, Container} from 'reactstrap';
 import './Login.css';
 import Example from './Example';
+import { getUserInfoField } from './Utility';
+import { getUserInfo } from './Utility';
+
+import $ from 'jquery';
 
 class Login extends React.Component{
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {
-            username: "xSour",
-            flag : false
-        }
-    }
+        this.username = React.createRef();
+        this.password = React.createRef();
+    }    
     
     submitHandler = (event) => {
         event.preventDefault();
-        console.log("sign in successful!!");
         //this.props.history.replace("/welcome");
-        this.setState({username: "Ghaeus"});
 
-        console.log(this.state.flag);
+        const u = this.username.current.value;
+        const p = this.password.current.value;
+
+        $.ajax({
+            url: "https://localhost:8443/app/api/login",
+            dataType: 'json',
+            type: 'POST',
+            data: {
+                username: u,
+                password: p
+            }
+        })
+        .then(json => {
+            console.log(json)
+            this.props.logInHandler(json.result.value);
+        })
+        .fail(err=>{
+            console.log(err)
+        })
     }
 
-    innerButton = () =>{
-        alert("button pressed!");
+    componentDidMount(){
+        console.log(getUserInfo());
+        console.log(this.props.role);
     }
+
+    // innerButton = () =>{
+    //     alert("button pressed!");
+    // }
 
     render(){
-        return(
-            <Container fluid id="content">
-                <Col>
-                    <Row className="justify-content-center">
-                        <h1>JUST BID IT</h1>
-                    </Row>
-                    <Row className="mb-3"/>
-                    <Row className="justify-content-center">
-                        {/* <Example username={this.state.username} onButtonClick={()=>{this.innerButton()}}/> */}
-                        <Col className="align-self-center" xs="auto">
-                            <Card id="login_form">
-                                <CardHeader>
-                                        Sign in
-                                </CardHeader>
-                                <CardBody>                                
-                                    <p className="small text-muted">Sign in to use our services</p>
-                                    <Form>
-                                        Username:
-                                        <input type="text" name="username"/>
-                                        Password:
-                                        <input type="text" name="password"/>
-                                        {/* <Button className="float-right font-weight-bold" id={classes.submit_btn}>Είσοδος</Button> */}
-                                        <Button onClick={this.submitHandler}>
+        if(this.props.role !== "guest"){
+            return(
+                <div>
+                    User is a {' '+this.props.role+' '} and has {' '+this.props.access+' '} access request
+                </div>
+            );
+        }
+        else{
+            return(
+                <Container fluid id="content">
+                    <Col>
+                        <Row className="justify-content-center">
+                            <h1>JUST BID IT</h1>
+                        </Row>
+                        <Row className="mb-3"/>
+                        <Row className="justify-content-center">
+                            {/* <Example username={this.state.username} onButtonClick={()=>{this.innerButton()}}/> */}
+                            <Col className="align-self-center p-0" xs="auto">
+                                <Card id="login_form">
+                                    <CardHeader>
                                             Sign in
-                                        </Button>
-                                    </Form>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                    </Row>
-                </Col>
-            </Container>
-        )
+                                    </CardHeader>
+                                    <CardBody>                                
+                                        <p className="small text-muted">Sign in to use our services</p>
+                                        <Form onSubmit={this.submitHandler}>
+                                            <Row>
+                                                <Col>Username:</Col>  
+                                                <Col><input type="text" name="username" ref={this.username}/></Col>   
+                                            </Row>
+                                            <Row>
+                                                <Col>Password:</Col>  
+                                                <Col><input type="password" name="password" ref={this.password}/></Col>   
+                                            </Row>                                        
+                                                                                    
+                                            <Row className="justify-content-center">
+                                                <Button type="submit">
+                                                    Sign in
+                                                </Button>
+                                            </Row>
+                                        </Form>
+                                    </CardBody>
+                                </Card>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Container>
+            );
+        }
+        
     }
 }
 
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,29 +1,54 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Redirect } from 'react-router-dom';
 import logo from './logo.svg';
+import { Row, Col, Button } from 'reactstrap';
+import { getUserInfoField } from './Utility';
+import { getUserInfo } from './Utility';
+
 
 class Welcome extends React.Component{
     constructor(props){
         super(props);
     }
 
+    redirectHandler = (url) => {
+        this.props.history.push(url);
+    }
+
     render(){
         return(
             <div>
 			    <header className="App-header">		
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
+                    <img src={logo} className="App-logo" alt="logo" />                                 
                 </header>
+                <body className="App-body">
+                    <Row className="d-flex justify-content-center">
+                        Hello dear 
+                        {' ' + (getUserInfoField("username") ? getUserInfoField("username") : "guest")}
+                        , welcome to JustBidIt
+                    </Row>
+                    <br/>
+                    <Row className="d-flex justify-content-center">
+                        <Col className="d-flex justify-content-end">
+                            <Button color="info" onClick={() => this.redirectHandler("/signup")}>
+                                Sign up
+                            </Button>
+                        </Col>
+                        <Col className="d-flex justify-content-start">
+                            <Button color="info" onClick={() => this.redirectHandler("/login")}>
+                                Sign in
+                            </Button>
+                        </Col>                        
+                    </Row>
+                    <br/>
+                    <Row >
+                        <Col className="d-flex justify-content-center">
+                            <Button color="info" onClick={() => this.redirectHandler("/search")}>
+                                Search
+                            </Button>
+                        </Col>
+                    </Row>     
+                </body>
             </div>
         )
     }

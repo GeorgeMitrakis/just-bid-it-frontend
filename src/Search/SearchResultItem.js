9cc4ab3ee5a3d87b24c955ea26da358a6b18aa56
George <georgemitr96@hotmail.com>
2019-09-27 19:33:04 +0300

    item edit oopsie

--- src/Search/SearchResultItem.js
+++ src/Search/SearchResultItem.js
@@ -278,7 +278,7 @@ class SearchResultItem extends React.Component{
                                 Highest Bid : $ {this.props.item.currentBid}
                             </span>
                             <span>
-                                {"$ "}<input className={bid+" "+ pairInput} type="number" step="0.5" min={this.props.item.currentBid+0.5} value={Number(this.state.bid)} onChange={(event)=> this.inputChangeHandler(event)}/>
+                                {"$ "}<input className={bid+" "+ pairInput} type="number" step="0.5" min={this.props.item.currentBid+0.5} value={this.state.bid} onChange={(event)=> this.inputChangeHandler(event)}/>
                                 <button type="button" color="muted" className={"btn btn-outline-secondary "+pairButton} onClick={()=>this.bidHandler()} > Bid</button>
                             </span>
                         </Row>
